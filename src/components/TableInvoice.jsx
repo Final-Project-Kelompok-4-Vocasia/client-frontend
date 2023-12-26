66da7ff32dc21976f0884550e237d4d95c6989b7
evaald <evaaldori239@gmail.com>
2023-12-26 14:02:26 +0700

    table invoice

--- src/components/TableInvoice.jsx
+++ src/components/TableInvoice.jsx
@@ -1,89 +1,57 @@
-function TableInvoice () {
-    return(
-        
+import React from "react";
 
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg p-10">
-      <table class="max-w-full w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-6 py-3">
-                    Product
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Qty
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Subtotal
-                </th>
-            </tr>
+function TableInvoice({ orderedMenus }) {
+  
+    // Pengecekan jika orderedMenus kosong atau undefined
+    if (orderedMenus.data_order.length === 0) {
+      return <p>No items to display.</p>;
+    }
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg p-10">
+      <table className="max-w-full w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+          <tr>
+            <th scope="col" className="px-6 py-3">
+              Product
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Quantity
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Subtotal
+            </th>
+          </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    Apple Watch
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    10
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    2
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    20
-                </td>
+          {orderedMenus.data_order.map((order, index) => (
+            <tr
+              key={index}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">{order.namaMenu}</td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">{order.qty}</td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">{order.harga}</td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">{order.subtotal}</td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    Apple Watch
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    10
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    2
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    20
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    Apple Watch
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    10
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    2
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    20
-                </td>
-            </tr>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    {/* blank */}
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    {/* blank */}
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    Total
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $999
-                </td>
-            </tr>
-
+          ))}
+          <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+            </td>
+            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+            </td>
+            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+              Total
+            </td>
+            <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">Rp.{orderedMenus.total_order}</td>
+          </tr>
         </tbody>
-    </table>
-</div>
-
-    );
-};
+      </table>
+    </div>
+  );
+}
 
 export default TableInvoice;
