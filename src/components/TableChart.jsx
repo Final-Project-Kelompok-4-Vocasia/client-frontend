1a2d00c69f99b07dd7a2fe17010f5472594b5c4f
evaald <evaaldori239@gmail.com>
2023-12-26 14:05:03 +0700

    table chart

--- src/components/TableChart.jsx
+++ src/components/TableChart.jsx
@@ -1,129 +1,156 @@
+import React, { useState, useEffect } from "react";
+import {
+  loadSelectedMenusFromLocalStorage,
+  saveData,
+  saveToLocalStorage,
+} from "../utils/localstorage";
+// import { menu } from "../utils/local";
 
-function TableChart () {
-    return(
-        
+const rupiah = (number) => {
+  return new Intl.NumberFormat("id-ID", {
+    style: "currency",
+    currency: "IDR",
+  }).format(number);
+};
 
-<div class="relative overflow-x-auto shadow-md sm:rounded-lg">
-      <table class="max-w-full w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-            <tr>
-                <th scope="col" class="px-16 py-3">
-                    <span class="sr-only">Image</span>
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Product
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Qty
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Price
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Sub Total
-                </th>
-                <th scope="col" class="px-6 py-3">
-                    Action
-                </th>
-            </tr>
+function TableChart({ selectedMenus}) {
+  // console.log(selectedMenu)
+  const [selectedMenu, setSelectedmenu] = useState(selectedMenus); // Initialize with 1
+
+  let tempTotal = 0;
+  const menus = selectedMenu.filter((item) => {
+    // let menus;
+    selectedMenu.map((el) => el.id).includes(item.id);
+    // for (let menu in selectedMenus) {
+    //   console.log(menu, "INIMENUT");
+    //   if (item.namaMenu === menu) {
+    //     item.qty = selectedMenu[menu];
+    //     item.subtotal = item.harga * item.qty;
+    //     tempTotal += item.subtotal;
+    //     menus = item;
+    // }
+    // }
+    // return menus;
+  });
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  const invoice = {
+    id_order: new Date().getTime(),
+    create_at: new Date().toLocaleString(),
+    data_order: menus,
+    total_order: tempTotal,
+    status_order: true,
+  };
+
+  let array_chart = loadSelectedMenusFromLocalStorage() ;
+  const handleQuantityChange = (id, value) => {
+    const index = array_chart.map((el) => el.id).indexOf(id);
+    if (index !== -1) {
+      array_chart[index].qty = value;
+    }
+
+    saveToLocalStorage(array_chart);
+    setSelectedmenu(array_chart);
+  };
+  array_chart.map((item)=>tempTotal += item.qty * item.harga)
+
+  function handleRemoveItem(id) {
+    // console.log(id)
+    const newarray = array_chart.filter((menus)=> menus.id !== id)
+    saveToLocalStorage(newarray)
+    setSelectedmenu(newarray)
+  }
+
+  // Effect untuk memperbarui subtotal saat quantities berubah
+  useEffect(() => {
+    saveData(invoice, "invoice");
+  }, [tempTotal]);
+
+  return (
+    <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+      <table className="max-w-full w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+        <thead>
+          <tr className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <th scope="col" className="px-6 py-3">
+              Image
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Product
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Qty
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Price
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Sub Total
+            </th>
+            <th scope="col" className="px-6 py-3">
+              Action
+            </th>
+          </tr>
         </thead>
         <tbody>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="p-4">
-                    <img src="/docs/images/products/apple-watch.png" class="w-16 md:w-32 max-w-full max-h-full" alt="Apple Watch"/>
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    Apple Watch
-                </td>
-                <td class="px-6 py-4">
-                    <div class="flex items-center">
-                        <div>
-                            <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required/>
-                        </div>
-                    </div>
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $599
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $10
-                </td>
-                <td class="px-6 py-4">
-                    <a href="/" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
-                </td>
+          {selectedMenu.map((item, index) => (
+            <tr
+              key={index}
+              className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+            >
+              <td className="p-4">
+                <img
+                  src={item.image}
+                  className="w-16 h-16 max-w-full max-h-full object-center"
+                  alt={item.namaMenu}
+                />
+              </td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                {item.namaMenu}
+              </td>
+              <td className="px-6 py-4">
+                <div className="flex items-center">
+                  <div>
+                    <input
+                      type="number"
+                      id={`quantity_${index}`}
+                      value={item.qty}
+                      onChange={(e) =>
+                        handleQuantityChange(item.id, parseInt(e.target.value))
+                      }
+                      className="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      required
+                    />
+                  </div>
+                </div>
+              </td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                {rupiah(item.harga)}
+              </td>
+              <td className="px-6 py-4 font-semibold text-gray-900 dark:text-white">
+                {rupiah(item.qty * item.harga)}
+              </td>
+              <td className="px-6 py-4">
+                <button
+                  onClick={() => handleRemoveItem(item.id)}
+                  className="font-medium text-red-600 dark:text-red-500 hover:underline"
+                >
+                  Remove
+                </button>
+              </td>
             </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="p-4">
-                    <img src="/docs/images/products/imac.png" class="w-16 md:w-32 max-w-full max-h-full" alt="Apple iMac"/>
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    iMac 27"
-                </td>
-                <td class="px-6 py-4">
-                <div class="flex items-center">
-                        <div>
-                            <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required/>
-                        </div>
-                    </div>
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $2499
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $10
-                </td>
-                <td class="px-6 py-4">
-                    <a href="/" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
-                </td>
-            </tr>
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="p-4">
-                    <img src="/docs/images/products/iphone-12.png" class="w-16 md:w-32 max-w-full max-h-full" alt="iPhone 12"/>
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    IPhone 12 
-                </td>
-                <td class="px-6 py-4">
-                <div class="flex items-center">
-                        <div>
-                            <input type="number" id="first_product" class="bg-gray-50 w-14 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block px-2.5 py-1 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="1" required/>
-                        </div>
-                    </div>
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $10
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $10
-                </td>
-                <td class="px-6 py-4">
-                    <a href="/" class="font-medium text-red-600 dark:text-red-500 hover:underline">Remove</a>
-                </td>
-            </tr>
-
-            <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    {/* blank */}
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    {/* blank */}
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    {/* blank */}
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    Total
-                </td>
-                <td class="px-6 py-4 font-semibold text-gray-900 dark:text-white">
-                    $999
-                </td>
-            </tr>
-
+          ))}
+          {/* Baris untuk menampilkan total */}
+          <tr className="text-xs font-semibold text-gray-900 dark:text-white">
+            <td colSpan="4" className="px-6 py-3 text-right">
+              Total
+            </td>
+            <td className="px-6 py-3">{rupiah(tempTotal)}</td>
+            <td className="px-6 py-3"></td>
+          </tr>
         </tbody>
-    </table>
-</div>
-
-    );
+      </table>
+    </div>
+  );
 }
 
 export default TableChart;
